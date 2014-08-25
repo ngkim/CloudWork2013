a8af3f17c2eeac4557d44c67a968bf2a76c5c32c
Namgon Kim <day10000@gmail.com>
2014-08-26 02:49:31 +0900

    update git-init.sh

--- git-init.sh
+++ git-init.sh
@@ -2,5 +2,5 @@
 
 git config --global user.email "day10000@gmail.com"
 git config --global user.name "Namgon Kim"
-git config --global push.default simple
+git config --global push.default matching
 
